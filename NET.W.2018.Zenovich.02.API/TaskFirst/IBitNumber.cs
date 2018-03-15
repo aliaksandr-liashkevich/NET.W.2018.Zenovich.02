4e914d05702705629ca335b69c0c28a77abc812d
azenovich <alexander.zenovich@gmail.com>
2018-03-15 15:09:14 +0300

    README and was created.  Comment was added in IBitNumber.

--- NET.W.2018.Zenovich.02.API/TaskFirst/IBitNumber.cs
+++ NET.W.2018.Zenovich.02.API/TaskFirst/IBitNumber.cs
@@ -8,6 +8,17 @@ namespace NET.W._2018.Zenovich._02.API.TaskFirst
 {
     public interface IBitNumber
     {
+        /// <summary>
+        /// Insert <paramref name="rightValue"/> into <paramref name="leftValue"/> from <paramref name="j"/>-th to <paramref name="i"/>-th bit
+        /// </summary>
+        /// <returns>
+        /// Result of inserting <paramref name="rightValue"/> into <paramref name="leftValue"/
+        /// </returns>
+        /// <param name="leftValue">A int number source</param>
+        /// <param name="rightValue">A int inserted number</param>
+        /// <param name="i">Position to</param>
+        /// <param name="j">Position from</param>
+        /// <returns></returns>
         int InsertNumber(int leftValue, int rightValue, int i, int j);
     }
 }
---
+++ README.md
@@ -0,0 +1,17 @@
+# NET.W.2018.Zenovich.01
+
+## Tasks
+
+1) Implement insertion algorithm of bit from j-th to i-th bit
+
+## Description
+
+> 1. Task InsertNumber
+
+  * API includes:
+    * IBitNumber
+  * Model includes:
+    * BitNumber
+  * Tests:
+    * BitNumberTest
+
